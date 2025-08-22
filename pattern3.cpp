cc33ed17a9649736a7301d0c2bed584bebc58210
Vanshika <vanshikaraje010@gmail.com>
2025-08-22 06:44:52 +0530

    Add files via upload

---
+++ pattern3.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print3(int n){
+    for(int i=1 ;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<< endl;
+    }
+}
+int main(){
+    int n;
+    cout<<" enter the value of n"<< endl;
+    cin>>n;
+    print3(n);
+}
